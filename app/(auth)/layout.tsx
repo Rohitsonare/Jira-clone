a88dd5218edc787ba3320addabbe2fe0894eb33d
Rohit Sonare <rohitsonare@MacBookAir.lan>
2024-12-30 11:33:24 +0530

    Enhance AuthLayout with navigation buttons for sign-in and sign-up

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,4 +1,10 @@
+"use client";
+
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import { usePathname } from "next/navigation";
+import path from "path";
 
 interface AuthLayoutprops{
     children: React.ReactNode;
@@ -7,11 +13,27 @@ interface AuthLayoutprops{
 
 
 const AuthLayout = ({children}: AuthLayoutprops) =>{
+    const pathname =usePathname();
+    const isSignIn = pathname === "/sign-in";
+
+
     return(
-        <div>
-            <Image src="/logo.svg" height={50} width={100} alt="logo"/>
+     <main className="bg-neutral-100 min-h-screen">
+      <div className="mx-auto max-w-screen-2xl p-4">
+       <nav className="flex justify-between items-center">
+         <Image src="/logo.svg" height={56} width={152} alt="logo"/>
+         <Button asChild variant="secondary">
+          <Link href={isSignIn ? "/sign-up" : "/sign-in"}>
+            {isSignIn? "Sign Up" : "Login"}
+          </Link>
+         </Button>
+        </nav>
+        <div className="flex flex-col items-center justify-center pt-4 md:pt-14">
             {children}
         </div>
+      </div>
+
+     </main>
     );
 };
 
