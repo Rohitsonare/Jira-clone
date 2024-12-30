5beefa212cd56d29b375fae95b470619fc2c9038
Rohit Sonare <rohitsonare@MacBookAir.lan>
2024-12-30 11:36:39 +0530

    Implement form validation in SignInCard using react-hook-form and zod

--- feauters/auth/components/sign-in-card.tsx
+++ feauters/auth/components/sign-in-card.tsx
@@ -1,7 +1,12 @@
+import { Schema, TypeOf, z } from "zod";
 import {FcGoogle} from "react-icons/fc";
 import {FaGithub} from "react-icons/fa";
+import { useForm } from "react-hook-form";
+import Link from "next/link";
+import { zodResolver} from "@hookform/resolvers/zod"
 
 import { DottedSeparator } from "@/components/dotted-separator";
+
 import { 
     Card,
      CardContent,
@@ -10,8 +15,35 @@ import {
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormMessage,
+} from "@/components/ui/form"
+
+
+const formSchema = z.object({
+    email: z.string().trim().email(),
+    password: z.string().min(1, "Requierd"),
+});
+
 
  export const SignInCard = () => {
+
+    const form = useForm<z.infer<typeof formSchema>> ({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+          email: "",
+          password: "",
+        },
+    });
+
+    const onSubmit = (values: z.infer<typeof formSchema>) => {
+        console.log(values);
+    }
+
     return(
         <Card className="h-full w-full md:w-[487px] border-none shadow-none">
             <CardHeader className="flex items-center text-center justify-center p-7">
@@ -19,39 +51,55 @@ import { Button } from "@/components/ui/button";
                     Welcome Back!
                 </CardTitle>
             </CardHeader>
-            <div className="px-7 ">
+            <div className="px-7 mb-2 ">
                 < DottedSeparator />
             </div>
             <CardContent className="p-7">
-                <form className="space-y-4">
-                    <Input 
-                    required
-                    type="email"
-                    value={""}
-                    onChange={() => {}}
-                    placeholder="Enter email address"
-                    disabled={false}
+                <Form {...form}>
+                <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+                    <FormField
+                      name="email"
+                      control={form.control}
+                      render={(field) => (
+                        <FormItem>
+                           <FormControl>
+                              <Input 
+                                {...field}
+                                type="email"
+                                placeholder="Enter email address"
+                              />
+                           </FormControl>  
+                           <FormMessage/>
+                       </FormItem>
+                      )}
                     />
-                    <Input 
-                    required
-                    type="password"
-                    value={""}
-                    onChange={() => {}}
-                    placeholder="Enter password"
-                    disabled={false}
-                    min = {8}
-                    max = {256}
+                    <FormField
+                      name="password"
+                      control={form.control}
+                      render={(field) => (
+                        <FormItem>
+                           <FormControl>
+                              <Input 
+                                {...field}
+                                type="password"
+                                placeholder="Enter your password"
+                              />
+                           </FormControl>  
+                           <FormMessage/>
+                       </FormItem>
+                      )}
                     />
                     <Button disabled = {false} size = "lg" className="w-full">
                         Login
                     </Button>
-                    
-                </form> 
+                    </form>
+                    </Form> 
 
             </CardContent>
             <div className="px-7 ">
                 < DottedSeparator />
             </div>
+            <form/>
             <CardContent className="p-7 flex flex-col gap-y-4">
 
                 <Button
@@ -74,6 +122,17 @@ import { Button } from "@/components/ui/button";
                     Login with Github
                 </Button>
 
+            </CardContent>
+                <div className="px-7 ">
+                    <DottedSeparator/>
+                </div>
+            <CardContent className="p-7 flex items-center justify-center">
+                <p>
+                    Don&apos;t have an account?
+                    <Link href="/sign-up">
+                    <span className="text-blue-600">&nbsp;sign up</span>
+                    </Link>
+                </p>
             </CardContent>
         </Card>
     );
